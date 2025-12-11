aa8c16812ca98554246a2090c86aa578b472ab9c
Dawid Draguła <dawid.dragula@gmail.com>
2025-12-11 17:22:01 +0100

    Refactored build process to allow of dts bundling.

---
+++ packages/grid-pro-react/rollup.config.js
@@ -0,0 +1,42 @@
+import dts from 'rollup-plugin-dts';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+const isExternal = (id) => [
+    'react',
+    'react-dom',
+    '@highcharts/grid-pro'
+].some(pattern => id.startsWith(pattern));
+
+export default [{
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/index.js',
+        format: 'esm',
+        sourcemap: false,
+    },
+    external: isExternal,
+    plugins: [
+        resolve(),
+        typescript({
+            tsconfig: './tsconfig.json',
+            outDir: 'dist',
+            declaration: false,
+            declarationMap: false,
+            sourceMap: false
+        }),
+    ]
+}, {
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/index.d.ts',
+        format: 'esm'
+    },
+    external: isExternal,
+    plugins: [
+        dts({
+            resolve: true,
+            respectExternal: true
+        })
+    ]
+}];
